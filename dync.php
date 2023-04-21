7638ddd3b9a5fc885ad8b80d9e942255aaf18b6d
Alexey Subbotin <alesu1543@gmail.com>
2023-04-21 13:01:44 +0300

    add mssql support

--- dync.php
+++ dync.php
@@ -1,18 +1,47 @@
 <?php
 // блок инициализации
 try {
-	$pdoSet = new PDO('mysql:host='.$_GET['address'].';port:'.$_GET['port'], $_GET['login'], $_GET['password']);
-	$pdoSet->query('SET NAMES utf8');
+	if ($_GET['type'] == 'MySQL') {
+		$pdoSet = new PDO('mysql:host='.$_GET['address'].';port:'.$_GET['port'], $_GET['login'], $_GET['password']);
+		$pdoSet->query('SET NAMES utf8');
+	}
+	if ($_GET['type'] == 'MSSQL')
+		$pdoSet = new PDO('odbc:DRIVER=FreeTDS;TDS_Version=8.0;SERVERNAME='.$_GET['address'].';PORT='.$_GET['port'].';UID='.$_GET['login'].';PWD='.$_GET['password'].';');
+
+	
+	if (isset($_GET['tables'])) {
+		$pdoSet->query('USE '.$_GET['tables']);
+	}
+	if (isset($_GET['base'])) {
+		$pdoSet->query('USE '.$_GET['base']);
+	}
 } catch (PDOException $e) {
 	print "Error!: " . $e->getMessage() . "<br/>";
 	die();
 }
 // динамическое получение значений из базы данных.
-if (isset($_GET['bases'])) $sql = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA";
-if (isset($_GET['tables'])) $sql = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='".$_GET['tables']."'";
+if (isset($_GET['bases'])) {
+	if ($_GET['type'] == 'MySQL')
+		$sql = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA";
+	if ($_GET['type'] == 'MSSQL')
+		$sql = "EXEC sp_databases";
+}
+if (isset($_GET['tables'])) {
+	if ($_GET['type'] == 'MySQL')
+		$sql = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='".$_GET['tables']."'";
+	if ($_GET['type'] == 'MSSQL')
+		$sql = "SELECT TABLE_SCHEMA+'.'+TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_CATALOG='".$_GET['tables']."'";
+}
 if (isset($_GET['rows'])) {
-	$sql_cols = "SHOW COLUMNS FROM ".$_GET['rows'];
-	$sql = "SELECT * FROM ".$_GET['rows'];
+	if ($_GET['type'] == 'MySQL')
+		$sql_cols = "SHOW COLUMNS FROM ".$_GET['rows'];
+	if ($_GET['type'] == 'MSSQL')
+		$sql_cols = "SELECT COLUMN_NAME AS Field FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_CATALOG+'.'+TABLE_SCHEMA+'.'+TABLE_NAME = '".$_GET['rows']."'";
+	
+//echo $sql_cols;
+	
+	$sql = "SELECT TOP ".$_GET['top']." * FROM ".$_GET['rows'];
+//echo $sql;
 }
 ?><!DOCTYPE html><html><head></head><body><div id='iTableGet'><?php
 if (isset($_GET['rows'])) {
@@ -26,13 +55,20 @@ $stmt = $pdoSet->query($sql);
 $resultMF = $stmt->fetchAll(PDO::FETCH_NUM);
 // перебор двухмерного массива [][] с числ. индексами.
 for ($i = 0; $i < Count($resultMF); ++$i) {
-	if (isset($_GET['rows'])) echo '<tr>';
-	for ($iCol = 0; $iCol < Count($resultMF[$i]); ++$iCol) {
-		if (isset($_GET['bases'])) echo '<div>'.$resultMF[$i][$iCol].'</div>';
-		if (isset($_GET['tables']))	echo '<li><div class="nav_point">'.$resultMF[$i][$iCol].'</div></li>';
-		if (isset($_GET['rows'])) echo '<td>'.$resultMF[$i][$iCol].'</td>';
+	if (isset($_GET['bases'])) echo '<div>'.$resultMF[$i][0].'</div>';
+	if (isset($_GET['tables']))	echo '<li><div class="nav_point">'.$resultMF[$i][0].'</div></li>';
+
+	if (isset($_GET['rows'])) {
+		echo '<tr>';
+		for ($iCol = 0; $iCol < Count($resultMF[$i]); ++$iCol) {
+			if ($resultMF[$i][$iCol] != null) {
+				echo '<td>'.$resultMF[$i][$iCol].'</td>';
+			} else {
+				echo '<td>NULL</td>';
+			}
+		}
+		echo '</tr>';		
 	}
-	if (isset($_GET['rows'])) echo '</tr>';
 }
 if (isset($_GET['rows'])) echo '</table>';
 ?></div><div id='iListStatus'>good</div></body></html>
